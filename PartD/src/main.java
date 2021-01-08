342b5c5a57ffdf0e970b48dabcd15cbca5037e6c
mattyNortz <mentaltoughnesspt@gmail.com>
2021-01-09 12:49:00 +1300

    Upload my favourite transportation

--- PartD/src/main.java
+++ PartD/src/main.java
@@ -5,6 +5,7 @@ public class main {
 		// TODO Auto-generated method stub
 	System.out.println("Hi, my name is Mathew");
 	System.out.println("My favourite colour is blue");
+	System.out.println("My favourite mode of transportaion is a car");
 
 	}
 
